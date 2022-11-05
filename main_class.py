741a507d6a7c7a86270c1b87dc0a4a3edeb994a6
Isah Jacob <Isahjacob0@gmail.com>
2022-11-05 08:56:17 +0100

    added a print statement that print the end of the quiz and display the final score out of total number of questions

--- main_class.py
+++ main_class.py
@@ -8,8 +8,12 @@ for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
-    question_store.append(new_question )
+    question_store.append(new_question)
 
 quiz = QuizBrain(question_store)
+
 while quiz.still_has_question():
     quiz.next_question()
+    print("You have completed the quiz")
+    print(f"You final score is: {quiz.score}/{quiz.question_number}")
+
