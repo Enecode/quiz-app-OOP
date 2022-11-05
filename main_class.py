4462bc6551250e2cb9c018bb25788c706f2a9b7f
Isah Jacob <Isahjacob0@gmail.com>
2022-11-05 16:29:34 +0100

    modified the question text and question answer to point to the open trivia api

--- main_class.py
+++ main_class.py
@@ -1,12 +1,11 @@
 from data import question_data
-from data import question_data
 from question_model import Question
 from quizeBrain import QuizBrain
 
 question_store = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_store.append(new_question)
 
